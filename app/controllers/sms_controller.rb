b0285a889a46fdb87579e5ba73d11fa8d5a45f6e
Mattan Griffel <mattangriffel@gmail.com>
2012-08-18 16:11:29 -0400

    Add dl link to javascript file

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,10 +3,11 @@ class SmsController < ApplicationController
   def send_sms
     @client = Twilio::REST::Client.new TWILIO['account_sid'], TWILIO['auth_token']
     @number = params[:number]
+    @download_url = params[:download_url]
     @client.account.sms.messages.create(
       :from => '+16464900357',
       :to => @number,
-      :body => 'Download the app to stay connected with family & close friends: http://path.com/d/1llEOt!'
+      :body => 'Download the app to stay connected with family & close friends: ' + @download_url
     )
     render :confirm
   end
