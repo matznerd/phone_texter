3f6bf1e04a717e36511f7ce47e3968bbb104e6cb
Mattan Griffel <mattangriffel@gmail.com>
2012-08-18 18:10:06 -0400

    some changes

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,12 +1,20 @@
 class AppsController < ApplicationController
-
+  
+  # GET /apps
+  # GET /apps.json
   def index
     @apps = App.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @apps }
+    end
   end
   
   def download
     @app = App.find(params[:id])
-    if request.user_agent =~ /iPhone|iPad/
+    
+    if request.user_agent =~ /iphone|ipad/
       redirect_to @app.ios_url
     elsif request.user_agent =~ /android/
       redirect_to @app.android_url
@@ -17,20 +25,35 @@ class AppsController < ApplicationController
     end
   end
 
+  # GET /apps/1
+  # GET /apps/1.json
   def show
     @app = App.find(params[:id])
 
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @app }
+    end
   end
 
+  # GET /apps/new
+  # GET /apps/new.json
   def new
     @app = App.new
 
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @app }
+    end
   end
 
+  # GET /apps/1/edit
   def edit
     @app = App.find(params[:id])
   end
 
+  # POST /apps
+  # POST /apps.json
   def create
     @app = App.new(params[:app])
     if @app.save
@@ -40,21 +63,31 @@ class AppsController < ApplicationController
     end
   end
 
+  # PUT /apps/1
+  # PUT /apps/1.json
   def update
     @app = App.find(params[:id])
 
+    respond_to do |format|
       if @app.update_attributes(params[:app])
-        redirect_to @app, notice: 'App was successfully updated.'
+        format.html { redirect_to @app, notice: 'App was successfully updated.' }
+        format.json { head :no_content }
       else
-        render action: "edit"
+        format.html { render action: "edit" }
+        format.json { render json: @app.errors, status: :unprocessable_entity }
       end
+    end
   end
 
+  # DELETE /apps/1
+  # DELETE /apps/1.json
   def destroy
     @app = App.find(params[:id])
     @app.destroy
 
-    redirect_to apps_url
+    respond_to do |format|
+      format.html { redirect_to apps_url }
+      format.json { head :no_content }
     end
   end
 end
